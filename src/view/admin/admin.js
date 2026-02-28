277d26233dbfebfdcfe803e6a6143641af60b056
marcusAires <gadelhamarcus27@gmail.com>
2026-02-28 06:26:05 -0300

    progresso da task1

--- src/view/admin/admin.js
+++ src/view/admin/admin.js
@@ -1,22 +1,22 @@
 const agenda = document.getElementById("agenda")
-
 const agendados = JSON.parse(localStorage.getItem("DB"))
 
+
+const horarios = 
+[
+    "09:00", "10:00", "11:00", "12:00",
+    "13:00", "14:00", "15:00", "16:00",
+    "17:00", "18:00"
+]
+
 agendados.forEach(agendado => {
-    agenda.innerHTML+=
-    `<tr>
-    <td>
-    ${agendado.nome}
-    </td>
-    <td>
-    ${agendado.data}
-    </td>
-    <td>
-    ${agendado.fone}
-    </td>
-    <td>
-    ${agendado.servicos}
-    </td>
-    </tr>
+    agenda.innerHTML +=
+     `  <p>${agendado.data}</p>
+        <p>${agendado.horario}</p>
+        <p>${agendado.nome}</p>
     `
 });
+
+
+//agendados.forEach(agendado => {
+//});
